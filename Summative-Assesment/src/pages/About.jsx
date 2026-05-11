e3557e42ad31214375ed9f4ffea730da8caefee0
timothydarrell14-glitch <timothydarrell14@gmail.com>
2026-05-11 05:30:53 +0300

    feat: enhance About page layout and add welcome message with images

--- Summative-Assesment/src/pages/About.jsx
+++ Summative-Assesment/src/pages/About.jsx
@@ -1,15 +1,34 @@
 export const About = () => {
-return(
-       <header>
+
+    return (
+        <>
+        <header>
             <div>
                 <h2>Brand Name</h2>
             </div>
-            <div>
+            <nav>
                 <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Contact Us</a>
-            </div>
+                <a href="#">About</a>
+                <a href="#">Contact Us</a>
+            </nav>
         </header>
+        <main>
+            <img id="ab-img1" src="https://skinscience.bluelagoon.com/cdn/shop/files/Product_Assortment_WP.jpg?crop=center&height=640&v=1734361395&width=1280"></img>
+            <h1 id="welcome">Welcome To This Page</h1>
+            <div id="about">
+                <div>
+                    <img src="https://img.freepik.com/premium-psd/skin-care-moisturizing-cosmetic-premium-products-with-blue-water-background_99236-274.jpg"></img>
+                </div>
+
+                <div>
+                    <p>"We believe that skincare should be as effortless as it is effective. Born from a desire to simplify your daily ritual, (Brand Name) brings you high-performance, plant-based formulas designed to protect and nourish your natural glow. We don’t believe in 'fixing' your skin; we believe in celebrating it. By combining clean ingredients with modern science, we’ve created a collection that feels as good as it looks. Welcome to the new standard of self-care—where transparency meets results, and every drop is a step toward your healthiest skin yet."</p>
+                </div>
+            </div>
+        </main>
+        </>
     )
 }
 
+{/* <img id="ab-img2" src="https://hips.hearstapps.com/hmg-prod/images/mhl-skincare-marlowe-352-lead-67f40877dee69.jpg?crop=0.854xw:1.00xh;0.0737xw,0&resize=1200:*"></img>
+            <img id="ab-img3" src="https://www.dotandkey.com/cdn/shop/articles/image_2_19c1bdd6-6d0d-4e18-bd14-543f68069d11.png?v=1740989311&width=650"></img>
+             */}
